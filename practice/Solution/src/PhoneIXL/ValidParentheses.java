c59bea8fb8f27880c0284f8070e6890a98a2e937
Adianldj <dianjinli2018@u.northwestern.edu>
2017-10-06 23:35:39 -0500

    add practice

---
+++ practice/Solution/src/PhoneIXL/ValidParentheses.java
@@ -0,0 +1,25 @@
+package PhoneIXL;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class ValidParentheses {
+	public boolean isValid(String s){
+		if(s == null || s.length() == 0){
+			return true;
+		}
+		Deque<Character> stack = new LinkedList<Character>();
+		for(int i = 0 ; i < s.length() ; i++){
+			if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+				stack.offerFirst(s.charAt(i));
+			}else if(!stack.isEmpty() && s.charAt(i) == ')' && stack.peekFirst() == '('){
+				stack.pollFirst();
+			}else if(!stack.isEmpty() && s.charAt(i) == ']' && stack.peekFirst() == '['){
+				stack.pollFirst();
+			}else if(!stack.isEmpty() && s.charAt(i) == '}' && stack.peekFirst() == '{'){
+				stack.pollFirst();
+			}
+		}
+		return stack.isEmpty();
+	}
+}
