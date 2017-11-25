1785b0e69f7fd26cae75b26e2575334203bf5002
Adianldj <dianjinli2018@u.northwestern.edu>
2017-11-25 18:07:47 -0500

    add practice

--- practice/Solution/src/MicrosoftOnsite/wordBreak.java
+++ practice/Solution/src/MicrosoftOnsite/wordBreak.java
@@ -1,5 +1,25 @@
 package MicrosoftOnsite;
 
-public class wordBreak {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class wordBreak {
+	 public boolean wordBreak(String s, List<String> wordDict) {
+	       boolean[] breakable = new boolean[s.length() + 1];
+	        Set<String> set = new HashSet<String>();
+	        for(String word : wordDict){
+	            set.add(word);
+	        }
+	        breakable[0] = true;
+	        for(int i = 1 ; i <= s.length() ; i++){
+	            for(int j = 0 ; j < i ; j++){
+	                if(breakable[j] && set.contains(s.substring(j , i))){
+	                    breakable[i] = true;
+	                    break;
+	                }
+	            }
+	        }
+	        return breakable[s.length()];
+}
 }
