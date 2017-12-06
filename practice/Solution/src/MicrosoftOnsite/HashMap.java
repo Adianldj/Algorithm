781fb404e9096e1b484d88a4d8a12b9af87e7bcd
Adianldj <dianjinli2018@u.northwestern.edu>
2017-12-05 18:09:33 -0600

    add practice

---
+++ practice/Solution/src/MicrosoftOnsite/HashMap.java
@@ -0,0 +1,12 @@
+package MicrosoftOnsite;
+
+//public class HashMap {
+//	private int size = 256;
+//	private Node bucketArray[] = new Node[size];
+//	public HashMap(){
+//		
+//	};
+//	public HashMap(int initialsize){
+//		this.size = initialsize;
+//	}
+//}
